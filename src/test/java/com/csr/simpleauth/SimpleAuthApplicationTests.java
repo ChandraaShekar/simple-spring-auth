15cee1095bffd98f74c9c0008c8d5bb5063aeb1c
Chandra Shekar Reddy <csreddy1998@gmail.com>
2023-05-30 16:42:05 -0500

    Spring Initial Project Setup

---
+++ src/test/java/com/csr/simpleauth/SimpleAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csr.simpleauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
